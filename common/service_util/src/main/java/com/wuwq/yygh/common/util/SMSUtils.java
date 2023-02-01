064ebf85040e4b4e33b1e7488cce1795ceed7a37
aquan99 <89380135+aquan99@users.noreply.github.com>
2023-02-01 16:57:38 +0800

    third commit

--- common/service_util/src/main/java/com/wuwq/yygh/common/util/SMSUtils.java
+++ common/service_util/src/main/java/com/wuwq/yygh/common/util/SMSUtils.java
@@ -24,7 +24,7 @@ public class SMSUtils {
         String host = "http://dingxin.market.alicloudapi.com";
         String path = "/dx/sendSms";
         String method = "POST";
-        String appcode = "5319bc7de9a649889ab6d78ff734d5cf";
+        String appcode = "";
         Map<String, String> headers = new HashMap<String, String>();
         //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
         headers.put("Authorization", "APPCODE " + appcode);
